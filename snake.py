7ccf28f22486467f54eb45cde215647be1561f30
Talal968N <talal.nuimy@hotmail.com>
2024-11-01 17:35:09 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,16 +1,19 @@
 from turtle import Turtle 
 
 class Snake:
-    def_init_(self):
-        self.sankes = []
+    def __init__(self):
+        self.snakes = []
         self.position = [(-40,0),(-20,0),(0,0)]
     
     def create_snake(self):
       new_snake = Turtle ("square")
-      new_snake.pencolor("white")
+      new_snake.color("white")
       new_snake.penup()
       new_snake.goto(self.position[i])
       snakes.append(new_snake)
     def move_snake(self)
-        pass
+        for i in range (len(self.snake)-1):
+            snake[i].goto(snake[i+1].pos())
+            
+            
     
