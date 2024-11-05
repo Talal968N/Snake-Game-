68dcc93b979b8fe72fdf9250497fa9a4db322015
Talal968N <talal.nuimy@hotmail.com>
2024-11-05 13:05:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ window = Screen ()
 window. setup (width=800, height=800)
 window. bgcolor ("black")
 
+sam = Snake ( )
+
+game_is_on = True 
+while game_is_on:
+    sam. move()
+
 
 
 window. exitonclick ()
